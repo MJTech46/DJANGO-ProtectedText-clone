b62c15a73a03c8d3fcf92bb1d9a9ac2088648b0a
MJTech46 <140804026+MJTech46@users.noreply.github.com>
2024-07-11 15:27:20 +0530

    model (DB) updated.

--- Text/models.py
+++ Text/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Page(models.Model):
     url = models.CharField(max_length=255, null=False, unique=True)
-    password = models.CharField(max_length=128, null=False) #use make_password() to convert to 128 char hash
+    #password = models.CharField(max_length=128, null=False) #passwords are handled my the js on client side
 
 class Tab(models.Model):
     page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name="tabs")
